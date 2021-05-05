c4e5f50e43af257a9b34b24d9723561e314fd234
TimeaFuntikova <83706590+TimeaFuntikova@users.noreply.github.com>
2021-05-05 14:12:23 +0200

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,18 @@
+import sk.uniza.fri.VtipneOkno;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: timka
+ * Date: 5. 5. 2021
+ * Time: 12:08
+ */
+public class Main {
+
+    public static void main(String[] args) {
+        //JOptionPane.showConfirmDialog(null, "Chceš cookie?");
+        //JOptionPane.showConfirmDialog(null, "heh.");
+
+        VtipneOkno okno = new VtipneOkno();
+        okno.zobraz();
+    }
+}
